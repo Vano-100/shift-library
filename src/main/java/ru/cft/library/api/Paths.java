9150a645caae65c85c6ca0600d1f78f6b1092018
anton-filatov <41286107+anton-filatov@users.noreply.github.com>
2019-01-31 10:03:48 +0700

    fix paths

--- src/main/java/ru/cft/library/api/Paths.java
+++ src/main/java/ru/cft/library/api/Paths.java
@@ -1,8 +1,12 @@
 package ru.cft.library.api;
 
-public final class Paths {
+final class Paths {
 
     static final String AUTHORS = "/authors";
     static final String BOOKS = "/books";
     static final String ID = "/{id}";
+
+    private Paths() {
+    }
+
 }
